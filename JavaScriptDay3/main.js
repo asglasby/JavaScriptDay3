0c65dce951f4e423f57cc8e734ca70bff5de2af6
asglasby <aisha.developer@gmail.com>
2015-04-15 13:58:21 -0400

    first commit

---
+++ JavaScriptDay3/main.js
@@ -0,0 +1,21 @@
+﻿var dogs = [];
+var forebaseUrl = "http://ccamlessons.firebaseio.com/Dogs.json";
+
+var Dog = function (name, breed, color) {
+    this.name = name;
+    this.breed = breed;
+    this.color = color;
+}
+
+var postAjax = function (dog) {
+    var request = new XMLHttpRequest();
+    request.open('POST', firebaseUrl, true);
+    request.onload = function () {
+        if (this.status >= 200 && this.status < 400) {
+            console.log("success");
+        } else {
+            console.error(this.response);
+        }
+    }
+    request.send(JSON.stringify(dog));
+}
